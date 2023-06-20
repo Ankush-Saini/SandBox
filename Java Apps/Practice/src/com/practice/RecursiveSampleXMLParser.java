a9d13f4a52bbedac377c64245820a166d54b3b0c
ANKUSH SAINI <ankushsaini88@gmail.com>
2023-06-20 23:28:25 +0530

    Update RecursiveSampleXMLParser.java

--- Java Apps/Practice/src/com/practice/RecursiveSampleXMLParser.java
+++ Java Apps/Practice/src/com/practice/RecursiveSampleXMLParser.java
@@ -1,9 +1,17 @@
-import java.io.File;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class RecursiveSampleXMLParser {
@@ -13,7 +21,53 @@ public class RecursiveSampleXMLParser {
 		SAXParserFactory factory = SAXParserFactory.newInstance();
 		try {
 			SAXParser saxParser = factory.newSAXParser();
-			tags.forEach(tag -> parse(tag, saxParser));
+			final String fileContent = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\r\n"
+					+ "  <soap:Header>\r\n"
+					+ "    <ResponseHeader xmlns=\"https://www.google.com/apis/ads/publisher/v202305\">\r\n"
+					+ "      <requestId>xxxxxxxxxxxxxxxxxxxx</requestId>\r\n"
+					+ "      <responseTime>1063</responseTime>\r\n" + "    </ResponseHeader>\r\n"
+					+ "  </soap:Header>\r\n" + "  <soap:Body>\r\n"
+					+ "    <getAdUnitsByStatementResponse xmlns=\"https://www.google.com/apis/ads/publisher/v202305\">\r\n"
+					+ "      <rval>\r\n" + "        <totalResultSetSize>2</totalResultSetSize>\r\n"
+					+ "        <startIndex>0</startIndex>\r\n" + "        <results>\r\n" + "          <id>2371</id>\r\n"
+					+ "          <name>RootAdUnit</name>\r\n" + "          <description></description>\r\n"
+					+ "          <targetWindow>TOP</targetWindow>\r\n" + "          <status>ACTIVE</status>\r\n"
+					+ "          <adUnitCode>1002372</adUnitCode>\r\n" + "          <inheritedAdSenseSettings>\r\n"
+					+ "            <value>\r\n" + "              <adSenseEnabled>true</adSenseEnabled>\r\n"
+					+ "              <borderColor>FFFFFF</borderColor>\r\n"
+					+ "              <titleColor>0000FF</titleColor>\r\n"
+					+ "              <backgroundColor>FFFFFF</backgroundColor>\r\n"
+					+ "              <textColor>000720</textColor>\r\n"
+					+ "              <urlColor>008000</urlColor>\r\n"
+					+ "              <adType>TEXT_AND_IMAGE</adType>\r\n"
+					+ "              <borderStyle>DEFAULT</borderStyle>\r\n"
+					+ "              <fontFamily>DEFAULT</fontFamily>\r\n"
+					+ "              <fontSize>DEFAULT</fontSize>\r\n" + "            </value>\r\n"
+					+ "          </inheritedAdSenseSettings>\r\n" + "        </results>\r\n" + "	<results>\r\n"
+					+ "          <id>2372</id>\r\n" + "          <name>RootAdUnit</name>\r\n"
+					+ "          <description></description>\r\n" + "          <targetWindow>TOP</targetWindow>\r\n"
+					+ "          <status>ACTIVE</status>\r\n" + "          <adUnitCode>1002372</adUnitCode>\r\n"
+					+ "          <inheritedAdSenseSettings>\r\n" + "            <value>\r\n"
+					+ "              <adSenseEnabled>true</adSenseEnabled>\r\n"
+					+ "              <borderColor>FFFFFF</borderColor>\r\n"
+					+ "              <titleColor>0000FF</titleColor>\r\n"
+					+ "              <backgroundColor>FFFFFF</backgroundColor>\r\n"
+					+ "              <textColor>000000</textColor>\r\n"
+					+ "              <urlColor>008000</urlColor>\r\n"
+					+ "              <adType>TEXT_AND_IMAGE</adType>\r\n"
+					+ "              <borderStyle>DEFAULT</borderStyle>\r\n"
+					+ "              <fontFamily>DEFAULT</fontFamily>\r\n"
+					+ "              <fontSize>DEFAULT</fontSize>\r\n" + "            </value>\r\n"
+					+ "          </inheritedAdSenseSettings>\r\n" + "        </results>\r\n" + "      </rval>\r\n"
+					+ "    </getAdUnitsByStatementResponse>\r\n" + "  </soap:Body>\r\n" + "</soap:Envelope>";
+
+			Map<String, String> finalValueMap = new HashMap<>();
+			tags.forEach(tag -> parse(tag, saxParser, fileContent, finalValueMap));
+			String modifiedContent = new String(fileContent);
+			for (Map.Entry<String, String> mapEntry : finalValueMap.entrySet()) {
+				modifiedContent = modifiedContent.replace(mapEntry.getKey(), mapEntry.getValue());
+			}
+			System.out.println(modifiedContent);
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -23,7 +77,7 @@ public class RecursiveSampleXMLParser {
 		}
 	}
 
-	private static void parse(String tag, SAXParser saxParser) {
+	private static void parse(String tag, SAXParser saxParser, String fileContent, Map<String, String> finalValueMap) {
 		String tagsList[] = tag.split("\\.");
 		int splitHolderValue = -1;
 		int startInd = tagsList[tagsList.length - 1].indexOf("{");
@@ -37,8 +91,8 @@ public class RecursiveSampleXMLParser {
 		final boolean isSplitIndex = startInd != -1;
 		final int splitIndex = splitHolderValue;
 		try {
-			File inputFile = new File("D:\\SandBox\\Java Apps\\sample_soap.xml.txt");
-
+//			File inputFile = new File("D:\\SandBox\\Java Apps\\sample_soap.xml.txt");
+			InputStream inputStream = new ByteArrayInputStream(fileContent.getBytes());
 			DefaultHandler handler = new DefaultHandler() {
 				boolean tagFlag[] = new boolean[tagsList.length];
 				String lastName;
@@ -81,7 +135,9 @@ public class RecursiveSampleXMLParser {
 				@Override
 				public void endDocument() throws SAXException {
 					matchingValues.forEach(value -> {
-						System.out.println(lastName + " " + replaceStringPlaceholder(value));
+						String finalValue = replaceStringPlaceholder(value);
+						System.out.println(lastName + " " + finalValue);
+						finalValueMap.put(createTag(value, lastName), createTag(finalValue, lastName));
 					});
 				}
 
@@ -90,19 +146,24 @@ public class RecursiveSampleXMLParser {
 						return originalValue;
 					else {
 						if (splitIndex >= 0)
-							return "$" + originalValue.substring(0, splitIndex) + "$"
-									+ originalValue.substring(splitIndex);
+							return originalValue.substring(0, splitIndex) + "$" + originalValue.substring(splitIndex)
+									+ "$";
 						else {
 							int finalInd = originalValue.length() + splitIndex;
-							return originalValue.substring(0, finalInd) + "$" + originalValue.substring(finalInd) + "$";
+							return "$" + originalValue.substring(0, finalInd) + "$" + originalValue.substring(finalInd);
 						}
 					}
 				}
+
+				private String createTag(String value, String tagName) {
+					return "<" + tagName + ">" + value + "" + "</" + tagName + ">";
+				}
 			};
 
-			saxParser.parse(inputFile, handler);
+			saxParser.parse(inputStream, handler);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 }
